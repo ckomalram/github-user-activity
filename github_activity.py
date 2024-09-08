64b83a7a7ab731817c49c2b6180211d4fc6074ac
Carlyle Komalram <glaw14@gmail.com>
2024-09-08 09:09:28 -0500

    happy path solution

--- github_activity.py
+++ github_activity.py
@@ -3,32 +3,9 @@ import sys
 import http.client
 import json
 
-# https://api.github.com/users/ckomalram/events
-def execute_api(username):
-    conn = http.client.HTTPSConnection("api.github.com")
-    endpoint = f"/users/{username}/events"
-    github_token = os.getenv("GITHUB_TOKEN") #change every 30 days from github account
-    if not github_token:
-        print("Error: GITHUB_TOKEN not defined. Please, configure it, on env.")
-        return
-    headers = {
-        "User-Agent": "PythonScript",  # GitHub requiere un User-Agent personalizado
-        "Authorization": f"token {github_token}"
-    }
-    conn.request("GET", endpoint, headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    
 
-    if res.status != 200:
-        print(f"Failed to fetch activity for user {username}. Status Code => {res.status}")
-        return
-    events = json.loads(data)
-    #print(events)
-    display_events(events)
-
-# TODO Recorrer los eventos y mostrar resumen
 def display_events(events):
+    
     if not events:
         print("No recent events found for this user.")
     
@@ -43,19 +20,85 @@ def display_events(events):
             print(f"- {action.capitalize()} an issue in {repo_name}")
         elif event_type == "WatchEvent":
             print(f"- Starred {repo_name}")
-        # Agregar más tipos de eventos según sea necesario
+        elif event_type == "ForkEvent":
+            print(f"- Forked {repo_name}")
+        elif event_type == "CreateEvent":
+            ref_type = event.get("payload", {}).get("ref_type")
+            ref_name = event.get("payload", {}).get("ref")
+            print(f"- Created {ref_type} {ref_name} in {repo_name}")
+        elif event_type == "DeleteEvent":
+            ref_type = event.get("payload", {}).get("ref_type")
+            ref_name = event.get("payload", {}).get("ref")
+            print(f"- Deleted {ref_type} {ref_name} from {repo_name}")
         else:
             print(f"- {event_type} in {repo_name}")
-        #print(f"Event Type => {event_type} -- Repo Name => {repo_name}")
 
+def decode_response(data):
+        try:
+            events = json.loads(data)
+            return events
+        except json.JSONDecodeError:
+            print(f"Failed to parse the response from GITHUB API")
+            return []         
+
+# https://api.github.com/users/ckomalram/events
+def execute_api(username):
+    conn = http.client.HTTPSConnection("api.github.com")
+    endpoint = f"/users/{username}/events"
+    github_token = os.getenv("GITHUB_TOKEN") #change every 30 days from github account
+
+    if not github_token:
+        print("Error: GITHUB_TOKEN not defined. Please, configure it, on env.")
+        return [] 
+    headers = {
+        "User-Agent": "PythonScript",  # GitHub requiere un User-Agent personalizado
+        "Authorization": f"token {github_token}"
+    }
+
+    try:
+        conn.request("GET", endpoint, headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+
+        if res.status == 404:
+            print(f"User '{username}' not found. Please check the username.")
+            return []
+        elif res.status == 403:
+            print("Rate limit exceeded or access forbidden. Please try again later.")
+            return []
+        elif res.status != 200:
+            print(f"Failed to fetch activity for user {username}. Status Code => {res.status}")
+            return []    
+
+        events = decode_response(data)    
+
+    except http.client.HTTPException as http_error:
+        print(f"An Http Error ocurred: {http_error}")
+        return []
+    except Exception as err:
+        print(f"An error ocurred: {err}")
+        return []
+    finally:
+        conn.close() 
+    
+    return events
 
 def run():
     if len(sys.argv) < 2:
         print("Usage: python3 github_activity.py <username>")
         return
+    
     username = sys.argv[1]
+    event_type = sys.argv[2] if len(sys.argv) > 2 else None
+
     print(f"Fetching activity for github user: {username}")
-    execute_api(username)
+    events = execute_api(username)
+    if event_type:
+        events = [event for event in events if event.get("type") == event_type]
+        # print(events)
+        print(f"Filtered events by type: {event_type}")
+    
+    display_events(events)
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,2 +1,49 @@
 # Github User Activity
 In this project, you will build a simple command line interface (CLI) to fetch the recent activity of a GitHub user and display it in the terminal. This project will help you practice your programming skills, including working with APIs, handling JSON data, and building a simple CLI application.
+
+## Requirements:
+- Python 3.x
+
+## Installation:
+    ```sh   
+    git clone
+    python3 -m venv env    
+    source env/bin/activate
+    cd github-user-activity
+    pip3 install -r requirements.txt
+    ``` 
+## Token
+You should go to your github account and create a classic repo token.
+- Use that in a VAR inside of activate File of your env folder (env/bin/activate).
+    ```sh  
+    export GITHUB_TOKEN='put_ur_token_'
+    ``` 
+[Example of stackoverflow](https://stackoverflow.com/questions/39056356/how-can-i-use-a-postactivate-script-using-python-3-venv)
+
+## Commands - CLI
+### List all events
+    ```sh  
+    python3 github_activity.py <username>
+    ```
+
+### Filter By event type
+    ```sh  
+    python3 github_activity.py <username> [CreateEvent|PushEvent|ForkEvent|IssuesEvent|WatchEvent|DeleteEvent]
+    ```
+
+## Project url
+- [github-activity](https://roadmap.sh/projects/github-user-activity)
+- [replit-github-activity](TODO_URL)
+
+## Contributing
+If you would like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch (git checkout -b feature/new-feature).
+3. Make your changes and commit them (git commit -am 'Add new feature').
+4. Push the branch to your fork (git push origin feature/new-feature).
+5. Open a new Pull Request.
+
+
+## License
+This project is licensed under the MIT License.
